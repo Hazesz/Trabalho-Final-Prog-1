32f6807f3afd5a1b749fb2495aa41b53b8b9b70f
Igor Henrique Koga Vigato <ihenriquekogavigato@gmail.com>
2024-11-28 11:06:29 -0300

    Estrutura Principal

--- trabalho.c
+++ trabalho.c
@@ -1,6 +1,53 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-int main(){
+    struct dados_de_consumo{
+        
+    };
+
+    void menu(){
+
+    }
+    
+    void registro(){
+
+    }   
+
+    void media(){
+        switch(){
+
+        }
+    }
+
+    void media_dia(){
+
+    }
+
+    void media_sem(){
+
+    }
+
+    void media_mensal(){
+        
+    }
+
+    void pico_de_consumo(){
+
+    }
+
+    void consumo_periodo(){
+
+    }
+
+    void analisar(){
+        
+    }
+
+    void relatório(){
+
+    }
+    
+int main(void){
     
 }
